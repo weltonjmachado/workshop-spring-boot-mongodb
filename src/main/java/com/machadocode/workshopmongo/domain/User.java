03f623778e3f919ca55684aefab5b26e980daa27
Welton Johni Machado <welton.jmachado@outlook.com>
2023-04-18 12:43:47 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/machadocode/workshopmongo/domain/User.java
@@ -0,0 +1,64 @@
+package com.machadocode.workshopmongo.domain;
+
+import java.util.Objects;
+
+public class User implements java.io.Serializable{
+    public static final long serialVersionUID = 1L;
+    private String id;
+    private String name;
+    private String email;
+
+    public User() {
+    }
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/machadocode/workshopmongo/resources/Resources.java
@@ -0,0 +1,26 @@
+package com.machadocode.workshopmongo.resources;
+
+import com.machadocode.workshopmongo.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class Resources {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        User maria = new User("1", "Maria Brown", "maria@gmail.com");
+        User alex = new User("2", "Alex Green", "alex@gmail.com");
+        List<User> list = new ArrayList<>();
+        list.addAll(Arrays.asList(maria, alex));
+        return ResponseEntity.ok().body(list);
+    }
+}
