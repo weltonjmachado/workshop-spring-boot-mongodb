72c4323cabbe4841aa5d7ff6c236985c30c74cf6
Welton Johni Machado <welton.jmachado@outlook.com>
2023-04-19 17:29:37 -0300

    Obtendo um usuário por id

--- src/main/java/com/machadocode/workshopmongo/resources/UserResource.java
+++ src/main/java/com/machadocode/workshopmongo/resources/UserResource.java
@@ -5,9 +5,7 @@ import com.machadocode.workshopmongo.dto.UserDTO;
 import com.machadocode.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,11 +17,16 @@ public class UserResource {
     @Autowired
     private UserService service;
 
-    @GetMapping
+    @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<UserDTO>> findAll(){
-
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
     }
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
 }
---
+++ src/main/java/com/machadocode/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.machadocode.workshopmongo.services.exception;
+
+import java.io.Serial;
+
+public class ObjectNotFoundException extends RuntimeException{
+    @Serial
+    private static final long serialVersionUID = 1L;
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
