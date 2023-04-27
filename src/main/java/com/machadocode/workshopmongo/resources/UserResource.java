121098380d55f40aff24c980b37927de6789d2d7
Welton Johni Machado <welton.jmachado@outlook.com>
2023-04-27 13:22:05 -0300

    Atualização do usuário com PUT

--- src/main/java/com/machadocode/workshopmongo/resources/UserResource.java
+++ src/main/java/com/machadocode/workshopmongo/resources/UserResource.java
@@ -37,6 +37,14 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
         User obj = service.fromDTO(objDto);
