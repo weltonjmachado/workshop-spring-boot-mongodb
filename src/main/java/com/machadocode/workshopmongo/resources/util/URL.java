a577fdb6408ed4f394797227ae01a71a3707e29e
Welton Johni Machado <welton.jmachado@outlook.com>
2023-04-28 18:20:39 -0300

    Consulta com vários critérios

--- src/main/java/com/machadocode/workshopmongo/resources/util/URL.java
+++ src/main/java/com/machadocode/workshopmongo/resources/util/URL.java
@@ -1,6 +1,7 @@
 package com.machadocode.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Date;
 
 public class URL {
     public static String decodeParam(String text) {
@@ -8,6 +9,15 @@ public class URL {
             return java.net.URLDecoder.decode(text, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             return "";
+        }
+    }
+    public static Date convertDate(String textDate, Date defaultValue) {
+        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT"));
+        try {
+            return sdf.parse(textDate);
+        } catch (java.text.ParseException e) {
+            return defaultValue;
         }
     }
 }
--- src/main/java/com/machadocode/workshopmongo/services/PostService.java
+++ src/main/java/com/machadocode/workshopmongo/services/PostService.java
@@ -7,6 +7,7 @@ import com.machadocode.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,5 +28,10 @@ public class PostService {
 
     public List<Post> findByTitle(String text){
         return repo.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
     }
 }
